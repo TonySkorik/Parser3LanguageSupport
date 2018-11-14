8e16e0ce48d8774942bf10aeb0404c9cd4bb4963
Anton Skorik <anton.skorik@cbsi.com>
2018-11-14 23:21:29 +0300

    Added code navigation builder

--- src/Parser3CodeNavigator.ts
+++ src/Parser3CodeNavigator.ts
@@ -6,6 +6,62 @@ import { Config } from './Config';
 
 const EndOfFileMarker : string = "=== END OF FILE ===";
 
+interface NavigationListElement{
+	NormalizedName : string;
+	Name : string;
+	Symbol : Symbol;
+}
+
+class CodeNavigationListBuilder{
+
+	private _navigationList = new Array<NavigationListElement>();
+
+	public constructor(symbols : Symbol[]){
+		let nav : NavigationListElement[] = new Array<NavigationListElement>();
+		let autoMethod : Symbol | undefined;
+		let postprocessMethod : Symbol | undefined;
+
+		symbols.forEach(v=>{
+			let isAutoOrPostprocess = false;
+			nav.push({NormalizedName : v.GetNormalizedName(), Name : v.Name, Symbol : v});
+		});		
+		
+		nav.sort(this.Compare);
+
+		this._navigationList = nav;
+	}
+
+	public GetNavigationList() : string[] {
+		let ret : Array<string>	 = new Array<string>();
+		this._navigationList.forEach(e=>{
+			ret.push(e.Name);
+		});
+
+		return ret;
+	}
+
+	public GetSybolByName(name : string) : Symbol | undefined{
+		let f = this._navigationList.find((val)=>{
+			return val.Name === name;
+		});
+		if(f){
+			return f.Symbol;
+		}else{
+			return undefined;
+		}
+	}
+
+	private Compare(a : NavigationListElement,b : NavigationListElement) : number{
+		if (a.NormalizedName < b.NormalizedName) {
+			return -1;
+		}
+		if (a.NormalizedName > b.NormalizedName){
+			return 1;
+		}
+		return 0;
+	}
+}
+
 export class Parser3CodeNavigator {
 	public async GoToMethodDeclaration(editor : vscode.TextEditor){
 		let methodDeclarations = SymbolHelper.GetMethodDeclarations(editor.document);
